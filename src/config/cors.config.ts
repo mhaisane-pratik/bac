dfd48b43e47abd48c73a0c19d84d495e89ce7c5a
Pratik <your-email@gmail.com>
2026-02-20 10:53:29 +0530

    Fixed CORS for production

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -1,12 +1,13 @@
 // src/config/cors.config.ts
 import cors from "cors";
-
 const corsOptions = {
-  origin: "http://localhost:5173", // Your frontend URL
+  origin: [
+    "http://localhost:5173",
+    "https://glittery-cranachan-2d7045.netlify.app"
+  ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "x-user-id"],
-  exposedHeaders: ["Content-Range", "X-Content-Range"],
 };
 
 export const corsMiddleware = cors(corsOptions);
